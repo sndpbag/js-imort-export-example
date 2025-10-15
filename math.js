9d21454b375218dfc79424f338810465967db7e5
sndp bag <sndpbagg@gmail.com>
2025-10-15 21:35:29 +0530

    example of js import and export

---
+++ math.js
@@ -0,0 +1,21 @@
+
+export const add = (a,b)=>{
+    return a+b ;
+}
+
+
+
+export const subtract = (a,b)=>{
+    return a-b ;
+}
+
+
+
+export const multiplication = (a,b)=>{
+    return a*b ;
+}
+
+
+export const divition = (a,b)=>{
+    return a/b ;
+}
---
+++ reult.js
@@ -0,0 +1,12 @@
+import { add, multiplication } from "./math.js";
+
+const result =add(20,30);
+
+console.log(result);
+
+
+const product = multiplication(20,6);
+console.log(product)
+
+
+
